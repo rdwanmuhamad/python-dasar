f2602fe2a3d332dedb6c7f35d2be91ed685b1bf6
rdwanmuhamad <mrdwn624@gmail.com>
2023-10-04 22:55:37 +0800

    mengenal variabel

---
+++ mengenal-variabel/main.py
@@ -0,0 +1,25 @@
+# variabel ada tempat menyimpan data
+
+# menaruh / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+print("nilai a = ", a)
+print("nilai x = ", x)
+print("nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 #dengan menggunakan underscore
+juta10 = 10000000 #ini boleh
+nilaiZ = 17.5 #ini boleh
+
+# pemanggilan kedua
+print("nilai a = ", a)
+a = 7
+print("nilai a = ", a)
+
+# assignment indirect
+b = a 
+print("nilai b = ", b)
+
