037c8bbbff85005720f222a71c549be0bb83ccd1
rdwanmuhamad <mrdwn624@gmail.com>
2023-10-11 15:15:17 +0800

    casting tipe data

---
+++ casting-tipe-data/main.py
@@ -0,0 +1,53 @@
+# casting aalah merubah satu tipe ke tipe lain
+
+# float
+print("=====INTEGER=====")
+data_int = 9
+print("data :", data_int, "type : ", type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+
+print("data :", data_float, "type : ", type(data_float))
+print("data :", data_str, "type : ", type(data_str))
+print("data :", data_float, "type : ", type(data_float))
+
+# float
+print("=====FLOAT=====")
+data_float = 9
+print("data :", data_float, "type : ", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+
+print("data :", data_int, "type : ", type(data_int))
+print("data :", data_str, "type : ", type(data_str))
+print("data :", data_bool, "type : ", type(data_bool))
+
+# boolean
+print("=====BOOLEAN=====")
+data_bool = True
+print("data :", data_bool, "type : ", type(data_bool))
+
+data_int = int(data_bool)
+data_str = str(data_bool)
+data_float = float(data_bool)
+
+print("data :", data_int, "type : ", type(data_int))
+print("data :", data_str, "type : ", type(data_str))
+print("data :", data_bool, "type : ", type(data_bool))
+
+# string
+print("=====STRING=====")
+data_str = "10"
+print("data :", data_str, "type : ", type(data_str))
+
+data_int = int(data_str)
+data_bool = bool(data_str)
+data_float = float(data_str)
+
+print("data :", data_int, "type : ", type(data_int))
+print("data :", data_bool, "type : ", type(data_bool))
+print("data :", data_bool, "type : ", type(data_bool))
